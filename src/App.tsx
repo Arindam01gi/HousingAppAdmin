5238b6770c81be311186e8e049b364fe8f2bbf56
Arindam Maiti <arindammaiti2018@gmail.com>
2023-12-06 20:27:10 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+   <>
+    <div className="items-center justify-center">
+    <h1 className="text-3xl  underline">
+      Test
+    </h1>
+    </div>
+   </>
+  );
+}
+
+export default App;
