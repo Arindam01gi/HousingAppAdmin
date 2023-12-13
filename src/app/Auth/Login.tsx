88ef6f668c89ffb24a2bf28b24c2a729fea7ecfc
Arindam Maiti <arindammaiti2018@gmail.com>
2023-12-13 20:37:48 +0530

    update in login page

--- src/app/Auth/Login.tsx
+++ src/app/Auth/Login.tsx
@@ -1,7 +1,33 @@
 import "./login.css";
 import login from "../../assets/images/login.svg";
+import {  useState } from "react";
+
+const generateRandomCode = () => {
+  const characters = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  const codeLength = 4;
+  let code = "";
+  for (let i = 0; i < codeLength; i++) {
+    const randomIndex = Math.floor(Math.random() * characters.length);
+    code += characters.charAt(randomIndex);
+  }
+  return code;
+};
 
 const Login = () => {
+  const [captchaValue, setCaptchaValue] = useState("");
+  const [generatedCode, setGeneratedCode] = useState(generateRandomCode());
+
+
+
+  const handleInputChange = (e: any) => {
+    setCaptchaValue(e.target.value.toUpperCase());
+  };
+
+  const handleRefreshCaptcha = () => {
+    setGeneratedCode(generateRandomCode());
+    setCaptchaValue("");
+  };
+
   return (
     <div className="flex justify-center items-center bg-gradient-to-r from-cyan-500 to-blue-500 container h-screen">
       <div className="w-6/12 h-4/6 flex shadow-lg bg-white ">
@@ -11,12 +37,7 @@ const Login = () => {
         <div className="ml-8 w-6/12 flex  flex-col justify-center items-start">
           <div className="font-medium text-xl"> Login to your account</div>
           <form className="bg-white w-full ">
-            <div className="mt-12 " 
-            // style={{ border: "1px solid black" }}
-            >
-              <label className="block text-gray-700 text-sm font-bold mb-2">
-                Username
-              </label>
+            <div className="mt-12 ">
               <input
                 className="shadow border rounded w-10/12 py-2 px-3 text-gray-700 "
                 id="username"
@@ -25,28 +46,53 @@ const Login = () => {
               />
             </div>
             <div className="mt-6 ">
-              <label className="block text-gray-700 text-sm font-bold mb-2">
+              {/* <label className="block text-gray-700 text-sm font-bold mb-2">
                 Password
-              </label>
+              </label> */}
               <input
                 className="shadow  w-10/12 border rounded  py-2 px-3 text-gray-700 mb-3"
                 id="password"
                 type="password"
-                placeholder="password"
+                placeholder="Password"
               />
             </div>
+            <div className="mt-3 ">
+              <input
+                className="shadow  w-10/12 border rounded  py-2 px-3 text-gray-700 mb-3"
+                id="society_code"
+                type="society_code"
+                placeholder="Society code"
+              />
+            </div>
+            <div className="mt-3 flex">
+              <input
+                className="shadow border rounded w-4/12 py-2 px-3  text-gray-700 "
+                id="captcha"
+                type="text"
+                placeholder="Captcha"
+                value={captchaValue}
+                onChange={handleInputChange}
+              />
+              <input
+                className="shadow border rounded w-3/12 py-2 px-3 ml-3 text-gray-700 bg-gray-200 "
+                type="text"
+                value={generatedCode}
+                readOnly
+              />
               <button
-                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded w-10/12 mt-6 focus:outline-none focus:shadow-outline"
+                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 m rounded ml-4 w-2/12 focus:outline-none focus:shadow-outline"
                 type="button"
+                onClick={handleRefreshCaptcha}
               >
-                Log In
+                <i className="fa-solid fa-arrows-rotate text-white"></i>
               </button>
-              {/* <a
-                className="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800"
-                href="#"
-              >
-                Forgot Password?
-              </a> */}
+            </div>
+            <button
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded w-10/12 mt-8 focus:outline-none focus:shadow-outline"
+              type="button"
+            >
+              Log In
+            </button>
           </form>
         </div>
       </div>
