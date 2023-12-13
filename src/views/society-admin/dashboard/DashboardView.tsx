ea0ad3c106dc247e1c71bb2c3af6d27e2b6fef62
Mainak231 <mainak04.das@gmail.com>
2023-12-14 01:15:59 +0530

    working on dashboard

--- src/views/society-admin/dashboard/DashboardView.tsx
+++ src/views/society-admin/dashboard/DashboardView.tsx
@@ -1,17 +1,20 @@
 const DashboardView = () => {
     return(
-        <>
-        <div className="shadow-xl w-2/12 mb-3 z-999 absolute">
-            <div className=" flex px-2 rounded-sm text-white text-lg bg-gradient-to-r from-cyan-500 to-blue-500 py-3">
-                Neighbourhood
+        <div className="grid grid-cols-12 h-screen">
+        <div className="shadow-xl z-40 col-span-2">
+            <div className="flex justify-between items-center px-3 text-white text-lg bg-gradient-to-r from-cyan-500 to-blue-500 py-3">
+                <span className="font-medium">Neighbourhood</span>
+                <i className="fa-solid fa-bars text-white"></i>
             </div>
          </div>
-        <div className="shadow-xl h-screen w-full mb-3 ">
-            <div className=" flex justify-center items-center rounded-sm text-white text-lg bg-gradient-to-r from-cyan-500 to-blue-500 py-3">
-            
+        <div className="shadow-xl bg-gray-50 col-span-10">
+            <div className=" flex items-center text-white text-lg bg-gradient-to-r from-cyan-500 to-blue-500 py-3">
+                <div>
+                    <i className="fa-solid fa-magnifying-glass text-white"></i>
+                </div>
             </div>
         </div>
-        </>
+        </div>
     )
 }
 export default DashboardView;
