b9ca2082f6ad750ddb38f2e30e50dd7a367ad1c7
Arindam Maiti <arindammaiti2018@gmail.com>
2023-12-20 10:07:32 +0530

    update in login page

--- src/views/login-registration/login/LoginView.tsx
+++ src/views/login-registration/login/LoginView.tsx
@@ -1,5 +1,7 @@
 import "./login.css";
 import login from "../../../assets/images/login.svg";
+import design from "../../../assets/images/design.svg";
+import logogif from "../../../assets/images/logogif.gif";
 import { useState } from "react";
 import { generateRandomCode } from "../../../utils";
 import { useForm } from "react-hook-form";
@@ -29,56 +31,63 @@ const LoginView = ({ loginAdmin }: LoginViewProps) => {
   };
 
   return (
-    <div className="flex justify-center items-center bg-gradient-to-r from-cyan-500 to-blue-500  h-screen w-screen">
-      <div className="w-6/12 h-4/6 flex shadow-lg bg-white ">
-        <div className="w-6/12 bg-blue-100 flex justify-center items-center">
-          <img src={login} alt="login" />
+    <>
+      <nav className="relative">
+        <div className="absolute top-2 left-0">
+          <img
+            src={design}
+            className="w-[240px] h-[80px] object-cover"
+            alt="Neighbourhood Logo"
+          />
         </div>
-        <div className="ml-8 w-6/12 flex  flex-col justify-center items-start">
+      </nav>
+      <div className="flex justify-between p-8">
+        <div className="ml-16 w-6/12 flex  flex-col justify-center my-48 ">
           <div className="font-medium text-xl">Welcome Back, Admin</div>
           <div className="font-small text-sm mt-2 text-gray-400">
-            {" "}
             Login to your account
           </div>
           <form className="bg-white w-full ">
-
             <div className="mt-8 ">
-              <CustomInput  
-                rules={{ ...requiredValidator, ...emailValidator}}
+              <CustomInput
+                rules={{ ...requiredValidator, ...emailValidator }}
                 name="admin_email"
                 control={control}
                 defaultValue={null}
                 errors={errors}
-                className="shadow w-10/12 border rounded  py-2 px-3 text-gray-700 mb-3"
+                className="shadow w-6/12 border rounded  py-2 px-3 text-gray-700 mb-3"
                 type="email"
-                placeholder="Email Address*"/>
+                placeholder="Email Address*"
+              />
             </div>
             <div className="mt-6 ">
-                <CustomInput  
-                  rules={{ ...requiredValidator}}
-                  name="admin_password"
-                  control={control}
-                  defaultValue={null}
-                  value={null}
-                  errors={errors}
-                  className="shadow w-10/12 border rounded  py-2 px-3 text-gray-700 mb-3"
-                  type="password"
-                  placeholder="Password*"/>
+              <CustomInput
+                rules={{ ...requiredValidator }}
+                name="admin_password"
+                control={control}
+                defaultValue={null}
+                value={null}
+                errors={errors}
+                className="shadow w-6/12 border rounded  py-2 px-3 text-gray-700 mb-3"
+                type="password"
+                placeholder="Password*"
+              />
             </div>
             <div className="mt-3 ">
-              <CustomInput  
-                  rules={{ ...requiredValidator}}
-                  name="apt_code"
-                  control={control}
-                  defaultValue={null}
-                  errors={errors}
-                  className="shadow w-10/12 border rounded  py-2 px-3 text-gray-700 mb-3"
-                  type="text"
-                  placeholder="Society code*"/>
+              <CustomInput
+                rules={{ ...requiredValidator }}
+                name="apt_code"
+                control={control}
+                defaultValue={null}
+                errors={errors}
+                className="shadow w-6/12 border rounded  py-2 px-3 text-gray-700 mb-3"
+                type="text"
+                placeholder="Society code*"
+              />
             </div>
             <div className="mt-3 flex">
               <input
-                className="shadow border rounded w-4/12 py-2 px-3  text-gray-700 "
+                className="shadow border rounded w-2/12 py-2 px-3  text-gray-700 "
                 id="captcha"
                 type="text"
                 placeholder="Captcha"
@@ -86,13 +95,13 @@ const LoginView = ({ loginAdmin }: LoginViewProps) => {
                 onChange={handleInputChange}
               />
               <input
-                className="shadow border rounded w-3/12 py-2 px-3 ml-3 text-gray-700 bg-gray-200 "
+                className="shadow border rounded w-2/12 py-2 px-3 ml-3 text-gray-700 bg-gray-200 "
                 type="text"
                 value={generatedCode}
                 readOnly
               />
               <button
-                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 m rounded ml-4 w-2/12 focus:outline-none focus:shadow-outline"
+                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4  rounded ml-12 w-1/12 focus:outline-none focus:shadow-outline"
                 type="button"
                 onClick={handleRefreshCaptcha}
               >
@@ -100,7 +109,7 @@ const LoginView = ({ loginAdmin }: LoginViewProps) => {
               </button>
             </div>
             <button
-              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded w-10/12 mt-8 focus:outline-none focus:shadow-outline"
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded w-6/12 mt-8 focus:outline-none focus:shadow-outline"
               type="button"
               onClick={handleSubmit(loginAdmin)}
             >
@@ -108,8 +117,13 @@ const LoginView = ({ loginAdmin }: LoginViewProps) => {
             </button>
           </form>
         </div>
+        <div className="flex justify-center items-center w-6/12">
+          <div className="">
+            <img src={logogif} alt="" className="w-6/12 bg-gray-700" style={{borderRadius:"50%"}} />
+          </div>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 export default LoginView;
